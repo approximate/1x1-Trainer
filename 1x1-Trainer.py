80276c3a94388efda5c566639881434798dfd7a1
Tim Zverev <dins.bums@gmail.com>
2020-02-16 11:51:14 +0100

    fix: Punkte

--- 1x1-Trainer.py
+++ 1x1-Trainer.py
@@ -4,10 +4,13 @@ while True:
     a = randint(1, 10)
     b = randint(1, 10)
     d = (a * b)
+    score = 0
     print(a, 'x', b, '= ', end='')
     c = int(input(''))
     if c == d:
         print('Richtig!')
+        score + 1
     else:
         print('Falsch!\nGame Over')
         break
+        print("Deine Punkte =", score)
